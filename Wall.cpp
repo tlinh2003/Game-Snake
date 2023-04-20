1cc642700173dd35702725b2f3cf22114bde380e
tlinh2003 <100563976+tlinh2003@users.noreply.github.com>
2023-04-20 20:49:04 +0700

    Add files via upload

---
+++ Wall.cpp
@@ -0,0 +1,24 @@
+#include "Wall.hpp"
+
+#include <stdlib.h>
+
+#include "Screen.hpp"
+
+namespace SnakeGame {
+
+const Uint8 Wall::S_WALL_RED = 0xFC;
+const Uint8 Wall::S_WALL_GREEN = 0x0F;
+const Uint8 Wall::S_WALL_BLUE = 0x08;
+
+const unsigned int Wall::S_WALL_WIDTH = 20;
+
+Wall::Wall(int x, int y) : Collideable(x, y) {}
+
+void Wall::draw(Screen& screen) {
+    for (int i = 0; i < S_WALL_WIDTH; i++)
+        for (int j = 0; j < S_WALL_WIDTH; j++)
+            screen.setPixel((int)m_x + i, (int)m_y + j, Wall::S_WALL_RED,
+                            Wall::S_WALL_GREEN, Wall::S_WALL_BLUE);
+}
+
+}  // namespace SnakeGame
